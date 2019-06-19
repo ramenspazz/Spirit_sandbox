23e20788b6eb35ccbfecffa940845fc93839fbbf
ramenspazz <kirisame.ramen@gmail.com>
2019-06-19 18:42:07 -0500

    Add files via upload

--- plot_out.py
+++ plot_out.py
@@ -5,17 +5,19 @@ import matplotlib.cm as cmx
 
 def Plot_Lattice(f_name, n_cols, n_rows):
 	###===============================================###
-	# Itterativly add temporary vectors pulled from     #
+	# Itterativly add temporary vectors pulled from		#
 	# output file to a color coded 2D vector field plot #
-	# where color represents the vectors' z-magnitude.  #
-	# 		color plot is O(n^2)                #
+	# where color represents the vectors' z-magnitude.	#
+	# 				color plot is O(n^2)				#
 	###===============================================###
 	with open(f_name, 'r') as fp:
 		#start file parsing and plotting
 		X = np.arange(-n_cols/2,n_cols/2)
 		Y = np.arange(-n_rows/2,n_rows/2)
-		U = np.zeros((n_rows, n_cols)) #initialize data containers
-		V = np.zeros((n_rows, n_cols)) #initialize data containers
+		#initialize data containers
+		U = np.zeros((n_rows, n_cols))
+		V = np.zeros((n_rows, n_cols))
+		M = np.zeros((n_rows, n_cols))
 		
 		count = 0 # initilize counter
 		for ln in fp:
@@ -30,9 +32,9 @@ def Plot_Lattice(f_name, n_cols, n_rows):
 				temp = [float(i) for i in ln.split()]
 				U[count / n_cols][count % n_cols] = temp[0]
 				V[count / n_cols][count % n_cols] = temp[1]
-				#C[count / n_cols, count % n_rows] = temp[2]
+				M[count / n_cols][count % n_cols] = (temp[2]+1)/2
 				count += 1
 				continue
-		q = plt.quiver(U, V, units='xy', angles='xy', pivot='tip', width=0.1, scale=1)
+		q = plt.quiver(U, V, M, units='xy', angles='xy', pivot='tip', width=0.15, scale=1)
 		plt.show()
 	return
